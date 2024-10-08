4ffe5ceaf17a84f93a1ec18ca750adfc1b371f26
Olaf1303 <olaf.koziara@gmail.com>
2024-10-08 20:13:27 +0200

    update user routes

--- routes/user.js
+++ routes/user.js
@@ -3,8 +3,8 @@ const {register} = require("../controllers/authController");
 var router = express.Router();
 
 /* GET users listing. */
-router.post('/user', register)
-router.get('/user', (req, res) => {
+router.post('/register', register)
+router.get('/', (req, res) => {
     res.status(200).json({usr: 'ss'})
 })
 
